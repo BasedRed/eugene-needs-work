ad37f82df2cc8efbbea2f4cb07b0b440f6c55cff
BasedRed <gussmandva@gmail.com>
2021-12-29 01:04:04 +0200

    tried to fix Safari scale by changing -webkit lines TRY 02

--- src/components/Game.js
+++ src/components/Game.js
@@ -245,7 +245,7 @@ const StyledGame = styled.div`
   }
 
   .container {
-    -webkit-transform: scaleX(-200%) scaleY(200%);
+    -webkit-transform: scale(scaleX(-2), scaleY(2));
     transform: scaleX(-200%) scaleY(200%);
 
     pointer-events: none;
